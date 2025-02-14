f773ee2d40839481cb62d8090704d68f2be71b2b
Mohith Kanthamneni <50568915+Mohith1@users.noreply.github.com>
2025-02-13 22:14:30 -0800

    Update can-place-flowers.py

--- can-place-flowers.py
+++ can-place-flowers.py
@@ -23,9 +23,15 @@ class Solution:
             # check if the current plot is empty
             if flowerbed[i] == 0:
                 # check if the left and right plot are empty
-                empty_left_plot = (i==0) or (flowerbed[i-1] == 0)
-                empty_right_plot = (i == len(flowerbed) - 1) or (flowerbed[i+1] == 0)
-                # If both plots are empty, we can plant a flower here.
+                empty_left_plot = (i==0) or (flowerbed[i-1] == 0) 
+                empty_right_plot = (i == len(flowerbed) - 1) or (flowerbed[i+1] == 0) '''Here although you might feel it might throw Index Out of Bounds error
+  as we trying to access flowerbed[i+1] it would not as, If i is at the last index, the second part of the condition (flowerbed[i+1] == 0) won't be evaluated due to short-circuiting in Python. 
+  Short-circuiting means the expression is evaluated from left to right, and if the first part is True, Python won't evaluate the second part at all.
+  
+&& (AND): If the first condition is false, the second condition is not evaluated.
+|| (OR): If the first condition is true, the second condition is not evaluated.'''
+                
+# If both plots are empty, we can plant a flower here.
                 if empty_left_plot and empty_right_plot:
                     flowerbed[i] = 1
                     count += 1
